0f51302607c530a599363cc600d7a4dc6969633b
xni-ke <114573199+xni-ke@users.noreply.github.com>
2022-09-28 19:02:31 +0800

    Create Exercise 3.3.py

---
+++ Chapter 3/Exercise 3.3.py
@@ -0,0 +1,52 @@
+
+###
+# Exercise 3: Write a program to prompt for a score
+# between 0.0 and 1.0. If the score is out of range,
+# print an error message. If the score is between
+# 0.0 and 1.0, print a grade using the following table:
+
+# Score   Grade
+# >= 0.9     A
+# >= 0.8     B
+# >= 0.7     C
+# >= 0.6     D
+# < 0.6     F
+
+# Enter score: 0.95
+# A
+
+# Enter score: perfect
+# Bad score
+
+# Enter score: 10.0
+# Bad score
+
+# Enter score: 0.75
+# C
+
+# Enter score: 0.5
+# F
+# Run the program repeatedly as shown above to test the various different values for input.
+###
+
+
+
+try:
+    grade = input('Enter Score:')
+    grade = float(grade)
+    if grade > 1.0 :
+        print ('Bad score')
+    elif grade >= 0.9:
+        print('A')
+    elif grade >= 0.8:
+        print('B')
+    elif grade >= 0.7:
+        print('C')
+    elif grade >= 0.6:
+        print('D')
+    elif grade < 0.6:
+        print('F')
+    else :
+        print('Bad score')
+except:
+    print('Bad score')
