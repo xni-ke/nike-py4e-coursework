1906e6664cbc250a93a1711edcd3fa9ba1041254
xni-ke <114573199+xni-ke@users.noreply.github.com>
2022-09-28 18:36:46 +0800

    Add files via upload

---
+++ Chapter 3/Exercise 3.2 alternate.py
@@ -0,0 +1,29 @@
+
+###
+# Exercise 1: Rewrite your pay computation to give the employee 1.5 times the hourly rate for hours worked above 40 hours.
+# Enter Hours: 45
+# Enter Rate: 10
+# Pay: 475.0
+###
+# code by dani
+
+try:
+    hours = input('Enter Hours:')
+    total_hours = int(hours)
+    rate = input('Enter Rate:')
+    rate = int(rate)
+
+    regular_hours = 40
+    if (total_hours < 40):
+        regular_hours = total_hours
+
+    pay = regular_hours * rate
+
+    if (total_hours > 40):
+        ot_hours = total_hours - regular_hours
+        rate = rate * 1.5
+        pay = pay + (ot_hours * rate)
+
+    print("Pay: ", pay)
+except:
+    print('Error, please enter numeric input')
